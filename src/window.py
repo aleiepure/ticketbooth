4c2ad0403f3bf630862664f6e02be1cf84cb4851
aleiepure <39530238+aleiepure@users.noreply.github.com>
2023-07-20 15:19:35 +0200

    Main window layout

--- src/window.py
+++ src/window.py
@@ -2,14 +2,37 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from gi.repository import Adw, Gtk
+from gi.repository import Adw, Gio, GLib, Gtk
 
 
 @Gtk.Template(resource_path='/me/iepure/ticketbooth/ui/window.ui')
 class TicketboothWindow(Adw.ApplicationWindow):
     __gtype_name__ = 'TicketboothWindow'
 
-    label = Gtk.Template.Child()
+    _stack = Gtk.Template.Child()
+
+    _settings = Gio.Settings(schema_id='me.iepure.ticketbooth')
+
+    def _sort_on_changed(self, new_state, source):
+        self.set_state(new_state)
+        Gio.Settings(schema_id='me.iepure.ticketbooth').set_string('view-sorting', str(new_state)[1:-1])
+
+    def _style_on_changed(self, new_state, source):
+        self.set_state(new_state)
+        Gio.Settings(schema_id='me.iepure.ticketbooth').set_string('view-style', str(new_state)[1:-1])
+
+    _actions = {
+        ('view-sorting', None, 's', f"'{_settings.get_string('view-sorting')}'", _sort_on_changed),
+        ('view-style', None, 's', f"'{_settings.get_string('view-style')}'", _style_on_changed),
+    }
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self._restore_settings()
+        self.add_action_entries(self._actions, self)
+
+    def _restore_settings(self):
+        self._settings.bind('win-width', self, 'default-width', Gio.SettingsBindFlags.DEFAULT)
+        self._settings.bind('win-height', self, 'default-height', Gio.SettingsBindFlags.DEFAULT)
+        self._settings.bind('win-maximized', self, 'maximized', Gio.SettingsBindFlags.DEFAULT)
+        self._settings.bind('win-tab', self._stack, 'visible-child-name', Gio.SettingsBindFlags.DEFAULT)
