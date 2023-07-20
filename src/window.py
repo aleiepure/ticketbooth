bd8b5342b9563613bbc0e3c52593a8d8eb34f0a0
aleiepure <39530238+aleiepure@users.noreply.github.com>
2023-07-20 15:49:29 +0200

    Added theme switcher

--- src/window.py
+++ src/window.py
@@ -4,12 +4,15 @@
 
 from gi.repository import Adw, Gio, GLib, Gtk
 
+from .widgets.theme_switcher import ThemeSwitcher
+
 
 @Gtk.Template(resource_path='/me/iepure/ticketbooth/ui/window.ui')
 class TicketboothWindow(Adw.ApplicationWindow):
     __gtype_name__ = 'TicketboothWindow'
 
     _stack = Gtk.Template.Child()
+    _menu_btn = Gtk.Template.Child()
 
     _settings = Gio.Settings(schema_id='me.iepure.ticketbooth')
 
@@ -30,6 +33,9 @@ class TicketboothWindow(Adw.ApplicationWindow):
         super().__init__(**kwargs)
         self._restore_settings()
         self.add_action_entries(self._actions, self)
+
+        # Theme (Adapted from https://gitlab.gnome.org/tijder/blueprintgtk/)
+        self._menu_btn.get_popover().add_child(ThemeSwitcher(), 'themeswitcher')
 
     def _restore_settings(self):
         self._settings.bind('win-width', self, 'default-width', Gio.SettingsBindFlags.DEFAULT)
